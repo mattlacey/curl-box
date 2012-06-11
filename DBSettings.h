01434a5bd3b4e5221364e49dcd76b7d0dce08c2a
mattlacey <laceysnr@gmail.com>
2012-06-11 16:18:31 +0000

    Added template DBSettings.h file, realised this would be handy when starting work on a Haiku machine.

---
+++ DBSettings.h
@@ -0,0 +1,17 @@
+#ifndef _DBSETTINGS_H
+#define _DBSETTINGS_H
+
+// Get your app key and secret from the Dropbox developer website
+#define OAUTH_KEY "" 
+#define OAUTH_SECRET "" 
+
+// ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
+#define APP_ACCESS_TYPE "dropbox"
+#define APP_ACCESS_TOKEN_URL ""
+
+#define APP_AUTH_URL ""
+#define APP_USER_AUTH_URL ""
+
+#endif
+
+
